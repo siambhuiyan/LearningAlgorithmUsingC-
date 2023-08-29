77ad07670cbb4fa93366a8c6d55e2bd6da0dd0c1
SiamBhuiyan <asadsiam588@yahoo.com>
2023-08-29 23:03:11 +0600

    using recursion checking sort or not array

---
+++ C++/Recursion/sortedornot.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool sortedOrNot(int arr[], int n)
+{
+    // base case
+    if (n == 0 || n == 1)
+    {
+        return true;
+    }
+
+    // check
+    if (arr[0] > arr[1])
+        return false;
+    // recursive call
+    return (sortedOrNot(arr + 1, n - 1));
+}
+
+int main()
+{
+    // code
+    int arr[] = {1, 4, 3, 4, 5};
+    cout << sortedOrNot(arr, 5) << endl;
+    bool sortOrNot = sortedOrNot(arr, 5);
+    if (sortOrNot)
+    {
+        cout << "The array is sorted." << endl;
+    }
+    else
+    {
+        cout << "The array is not sorted" << endl;
+    }
+    return 0;
+}
