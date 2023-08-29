350cc6c32482f77ff012e03114545916f2223857
SiamBhuiyan <asadsiam588@yahoo.com>
2023-08-29 23:16:13 +0600

    updated with different approach

--- C++/Recursion/sortedornot.cpp
+++ C++/Recursion/sortedornot.cpp
@@ -16,6 +16,22 @@ bool sortedOrNot(int arr[], int n)
     return (sortedOrNot(arr + 1, n - 1));
 }
 
+bool backwardCheck(int arr[], int n)
+{
+    // base
+    if (n == 0 || n == 1)
+    {
+        return true;
+    }
+    // check
+    if (arr[n] < arr[n - 1])
+    {
+        return false;
+    }
+    // recursive call
+    return backwardCheck(arr, n - 1);
+}
+
 int main()
 {
     // code
@@ -23,6 +39,16 @@ int main()
     cout << sortedOrNot(arr, 5) << endl;
     bool sortOrNot = sortedOrNot(arr, 5);
     if (sortOrNot)
+    {
+        cout << "The array is sorted." << endl;
+    }
+    else
+    {
+        cout << "The array is not sorted" << endl;
+    }
+
+    bool checkSorted = backwardCheck(arr, 5);
+    if (checkSorted)
     {
         cout << "The array is sorted." << endl;
     }
